ef18afb8549ea4ff71066bb72aa5e87518bb9948
Sebastien kusz <kusz.sebastien@gmail.com>
2021-01-23 20:56:48 +0100

    React 03 - Show a list of components

--- src/App.js
+++ src/App.js
@@ -1,31 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
-import Contact from './components/Contact';
+import ContactList from './components/ContactList';
 
 function App() {
   return (
     <div className="App">
       <div>
-        <Contact 
-          name = 'Sally Diaz'
-          avatar = 'https://randomuser.me/api/portraits/women/93.jpg'
-          online
-        />
-      </div>
-      <div>
-        <Contact 
-          name = 'Delores Martin'
-          avatar = 'https://randomuser.me/api/portraits/women/57.jpg'
-          online
-        />
-      </div>      
-      <div>
-        <Contact 
-          name = 'Vera Cole'
-          avatar = 'https://randomuser.me/api/portraits/women/29.jpg'
-          online
-        />
+        <ContactList />
       </div>
     </div>
   );
